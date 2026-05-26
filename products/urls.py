f7060a00ee7ae81760016d5c9cced1c69f1b684f
eleonora2687 <trikidoue@gmail.com>
2026-05-26 15:32:24 +0300

    Add product detail page and basic cart functionality with session-based cart

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import product_list
+from .views import product_list, product_detail, add_to_cart, cart_view, remove_from_cart
 
 urlpatterns = [
     path('', product_list, name='product_list'),
+    path('product/<int:id>/', product_detail, name='product_detail'),
+
+    path('cart/', cart_view, name='cart_view'),
+    path('cart/add/<int:id>/', add_to_cart, name='add_to_cart'),
+    path('cart/remove/<int:id>/', remove_from_cart, name='remove_from_cart'),
+
+
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404 # type: ignore
 from .models import Product
 
 def product_list(request):
@@ -6,4 +6,56 @@ def product_list(request):
 
     return render(request, 'products/product_list.html', {
         'products': products
+
+    })
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+
+    return render(request, 'products/product_detail.html', {
+        'product': product
+    })
+
+def add_to_cart(request, id):
+    cart = request.session.get('cart', {})
+
+    if str(id) in cart:
+        cart[str(id)] += 1
+    else:
+        cart[str(id)] = 1
+
+    request.session['cart'] = cart
+
+    return redirect('cart_view')
+
+def remove_from_cart(request, id):
+    cart = request.session.get('cart', {})
+
+    if str(id) in cart:
+        del cart[str(id)]
+
+    request.session['cart'] = cart
+
+    return redirect('cart_view')
+
+
+
+def cart_view(request):
+    cart = request.session.get('cart', {})
+
+    products = []
+    total = 0
+
+    for product_id, quantity in cart.items():
+        product = Product.objects.get(id=product_id)
+
+        product.total_price = product.price * quantity
+        product.quantity = quantity
+
+        total += product.total_price
+        products.append(product)
+
+    return render(request, 'products/cart.html', {
+        'products': products,
+        'total': total
     })
